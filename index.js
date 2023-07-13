74609d6195fef2c6a21434ad2080e960b6678844
xdLucky <julek.wojciak@gmail.com>
2023-07-13 14:28:10 -0700

    imported a new library + cant figure out what the library is doing even
    god help us

--- index.js
+++ index.js
@@ -5,20 +5,39 @@ const input = document.getElementById("input")
 const secondLog = document.getElementById("secondLog")
 const confirmer = document.getElementById("confirmer")
 var log = document.getElementById("log")
+const dayToday = Date.today()
+const days = new Map();
+days.set ("Mon", 1);
+days.set ("Tue", 2);
+days.set ("Wed", 3);
+days.set ("Thu", 4);
+days.set ("Fri", 5);
+days.set ("Sat", 6);
+days.set ("Sun", 7);
 
+
+// class Days {
+//     constructor(mood) {
+//         map1.get(this.id) = mood
+//     }
+
+// };
 
 day1.addEventListener("click", 
     function() {
         informer.show();
-        
-        
-        // document.getElementById("log").ninerHTML = this.id;
 });
+function findDayOfWeek(dateF) {
+    toString(dateF);
+    console.log(days.get(dateF.substring(0, 3)));
+    
+     return days.get(dateF.substring(0, 3));
+     
+};
 
 input.onclick = function(event){
     log.innerHTML = event.target.id;
     map1.set (this.id, event.target.id);
-    //secondLog.innerHTML = map1.get("2023/01/01");
     confirmer.show();
     if (event.target.id === "input"){
         log.innerHTML = "0";
@@ -26,7 +45,11 @@ input.onclick = function(event){
    
 };
 confirmer.onclick = function(event){
+    console.log(Date.today + 'i love little boys');
     confirmer.close();
     informer.close();
-    secondLog.innerHTML = "true"
+    console.log(toString(dayToday).substring(0) + 'substring');
+    console.log(dayToday + 'obj');
+    console.log(findDayOfWeek(Date.today()) + 'myfunc');
+    
 };
