0ad3d8cde6d2eaa659479d80e8848357d4b7dd9a
xdLucky <julek.wojciak@gmail.com>
2023-07-14 14:57:44 -0700

    made months they have 5 rows of days only one for jan though-- add monthly names

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const secondLog = document.getElementById("secondLog")
 const confirmer = document.getElementById("confirmer")
 var log = document.getElementById("log")
 const dayToday = Date.today()
-const days = new Map();
-days.set ("Mon", 1);
-days.set ("Tue", 2);
-days.set ("Wed", 3);
-days.set ("Thu", 4);
-days.set ("Fri", 5);
-days.set ("Sat", 6);
-days.set ("Sun", 7);
+//------------------------------------------------------------------------------------------------------------
+const days = new Map();             //setting days for function findDayOfWeek
+days.set ("Mon", 1);                //                               |
+days.set ("Tue", 2);                //                               |
+days.set ("Wed", 3);                //                               |
+days.set ("Thu", 4);                //                               |
+days.set ("Fri", 5);                //                               |
+days.set ("Sat", 6);                //                              \| /
+days.set ("Sun", 7);                //                               \/
+//------------------------------------------------------------------------------------------------------------
+function findDayOfWeek(dateF) {                           // return us the numerical number of day placement:
+    return days.get(dateF.toString().substring(0, 3));    // Mon Tues Wed Thu Fri Sat Sun |
+};                                                        //  1   2    3   4   5   6   7 
+//------------------------------------------------------------------------------------------------------------
 
 
 // class Days {
@@ -27,14 +33,7 @@ day1.addEventListener("click",
     function() {
         informer.show();
 });
-function findDayOfWeek(dateF) {
-    toString(dateF);
-    console.log(days.get(dateF.substring(0, 3)));
-    
-     return days.get(dateF.substring(0, 3));
-     
-};
-
+                                                          
 input.onclick = function(event){
     log.innerHTML = event.target.id;
     map1.set (this.id, event.target.id);
@@ -44,12 +43,15 @@ input.onclick = function(event){
     } // doesnt matter we will have no spaces 
    
 };
-confirmer.onclick = function(event){
-    console.log(Date.today + 'i love little boys');
+confirmer.onclick = function() {
     confirmer.close();
     informer.close();
-    console.log(toString(dayToday).substring(0) + 'substring');
-    console.log(dayToday + 'obj');
-    console.log(findDayOfWeek(Date.today()) + 'myfunc');
-    
 };
+
+
+// ------------------how to use the library-----------------------
+// console.log(dayToday.toString().substring(0) + 'substring');---
+// console.log(dayToday + 'obj');---------------------------------
+// console.log(findDayOfWeek(Date.today()) + 'myfunc');-----------
+// console.log(Date.today() + 'i love little boys');--------------
+//----------------------------------------------------------------
